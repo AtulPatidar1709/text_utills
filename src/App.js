2ad4b1596c2fdf25c24fbaf3fbc9509a8209cf96
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2023-01-22 12:38:43 +0530

    Connect BootStrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="container">
-      <p>Hello How are You</p>
-    </div>
+    
     </>
   );
 }
