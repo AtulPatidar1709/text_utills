d967414f8c2a4c4663630bf514162100576e523f
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2023-01-23 16:10:10 +0530

    App Changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+
 
 function App() {
+
+  const [changeMode, setchangeMode] = useState('ligth');
+
+  const toggleMode = () => {
+    if (changeMode === 'light') {
+      setchangeMode('dark');
+    }
+    else {
+      setchangeMode('light');
+    }
+
+  }
+
   return (
     <>
-      <Navbar title="TextUtiles" home="Home" about="About TextUtils" />
+      <Navbar title="TextUtiles" toggleMode={toggleMode} mode={changeMode} home="Home" about="About TextUtils" />
       <div className="container">
- 
-        <TextForm  heading="Enter a Text to Analyze below"/>
-    </div>  
-      {/* <Button button = "Convert to LowerCase" />
-      <Button button = "Remove Extra Space" /> */}
 
+        <TextForm heading="Enter a Text to Analyze below" />
+        {/* <About/> */}
+      </div>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,29 +2,62 @@ import './TextForm.css';
 import React, { useState } from 'react'
 export default function TextForm(props) {
 
-const handleOnChange = () => {
-   console.log("Upper was Clicked");
-}
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
 
-const handleOnClick = () => {
-    console.log("On Click");
-}
+    const handleOnUpperCase = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+
+    }
+
+    const handleOnLowerCase = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+
+    }
+
+    const handleOnErase = () => {
+        let newText = "";
+        setText(newText);
+
+    }
+
+    const removeExtraSpace = ( )  => {
+        let newText =  text.replace(/\s+/g, ' ').trim();
+        setText(newText)
+    }
+    
+    const copyFun = () =>  {
+        let textarea = document.getElementById("myBox");
+        textarea.select();
+        navigator.clipboard.writeText(textarea.value);
+      }
+
 
     const [text, setText] = useState('Enter your Text here...');
-
 
     return (
         <>
             <h2 className="heading my-3">{props.heading}</h2>
             <div className="mb-3">
-            <div className="container">  
-                <textarea className="form-control my-3" value={text} onChange =  {handleOnChange}   id="myBox" rows="8"></textarea>
+                <div className="container">
+                    <textarea className="form-control my-3" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
             </div>
+            <button className='btn' onClick={handleOnUpperCase}>Convert To Upper Case</button>
+            <button className='btn' onClick={handleOnLowerCase} >Convert To Lower Case</button>
+            <button className='btn' onClick={handleOnErase}>Erase All</button>
+            <button className='btn ' onClick={removeExtraSpace}>Extra Space Remover</button>
+            <button className='btn ' onClick={copyFun}>Copy</button>
+            <div className="container my-4">
+                <h2>Your Text Summery</h2>
+                <p>{text.split(" ").length} words and {text.length} characters.</p>
+                <p>{0.008 * text.split(" ").length} Minutes read.</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
             </div>
-            <button className='btn'  onClick={handleOnClick}>Convert To Upper Case</button>
-            <button className='btn'>Convert To Lower Case</button>
-            <button className='btn'>Remove Extra Space</button>
-
         </>
     )
 }
