8fd8f3f2b0af6139a6d1f743f170782a233e509c
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2023-01-30 22:12:31 +0530

    Changes in Dark Modes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 function App() {
 
@@ -12,21 +12,25 @@ function App() {
   const toggleMode = () => {
     if (changeMode === 'light') {
       setchangeMode('dark');
+      document.body.style.backgroundColor = 'grey';
     }
     else {
       setchangeMode('light');
+      document.body.style.backgroundColor = 'white'
     }
-
   }
 
   return (
     <>
-      <Navbar title="TextUtiles" toggleMode={toggleMode} mode={changeMode} home="Home" about="About TextUtils" />
-      <div className="container">
-
-        <TextForm heading="Enter a Text to Analyze below" />
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtiles" toggleMode={toggleMode} mode={changeMode} home="Home" about="About TextUtils" />
+        <div className="container">
+          <Routes>
+            <Route exact  path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm heading="Enter a Text to Analyze below" />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
     return (
@@ -13,15 +14,15 @@ export default function Navbar(props) {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
+                                <Link className="nav-link active" aria-current="page" to="/">{props.home}</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">{props.about}</a>
+                                <Link className="nav-link" to="/about">{props.about}</Link>
                             </li>
                         </ul>
-                        <div className={`form-check form-switch text-ligth`}>
-                            <input className="form-check-input" onClick={props.toggleMode}  type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label "httmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+                        <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                            <input className="form-check-input text-dark" onClick={props.toggleMode}  type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label "htmlFor="flexSwitchCheckDefault">{`Enable ${props.mode === 'dark' ? 'ligth' : 'dark'  } Mode`}</label>
                         </div>
                     </div>
                 </div>
